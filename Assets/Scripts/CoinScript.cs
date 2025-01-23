2a503c4a28cbda0cac672e751b91698b11a4e394
Joel Iglesias <joeltorminecraft@gmail.com>
2025-01-23 20:34:00 +0100

    Diamond and Coin Effect
    The effect of collectables when are picked.

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
+    private Animator animator;
     public int coinValue = 5;
 
     public static Action<CoinScript> OnCoinCollected;
 
+<<<<<<< Updated upstream
+=======
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+>>>>>>> Stashed changes
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            OnCoinCollected?.Invoke(this);
-            Destroy(gameObject);
+            animator.SetTrigger("coinCollected");
         }
+    }
+
+    private void DestroyCoin()
+    {
+        OnCoinCollected?.Invoke(this);
+        Destroy(gameObject);
     }
 }
